03ce04555f13cfce031a4f2eef8b537dad2892eb
mirrun13 <mirrun13@gmail.com>
2026-05-19 13:40:04 +0900

    Update history.js

--- api/history.js
+++ api/history.js
@@ -1,4 +1,8 @@
-import { kv } from '@vercel/kv';
+import { Redis } from '@upstash/redis';
+const kv = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 async function verifyToken(token) {
   if (!token) return false;
