3a6222f996f1054f6024a55b492b4028e73737f4
mirrun13 <mirrun13@gmail.com>
2026-05-19 13:47:06 +0900

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -3,10 +3,6 @@ const kv = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
-const kv = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
